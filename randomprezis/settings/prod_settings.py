e83f9dbd4aa22a5e63536e22b85fc99dbd8cc490
teglanna <teglanna@gmail.com>
2017-12-22 12:18:57 +0100

    Set up staticfiles

--- randomprezis/settings/prod_settings.py
+++ randomprezis/settings/prod_settings.py
@@ -10,3 +10,5 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
