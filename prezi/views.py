ef117180cbce1ef35a08b9b3fb0a248a6bcc07df
teglanna <teglanna@gmail.com>
2017-12-22 11:17:41 +0100

    Added basic search

--- prezi/views.py
+++ prezi/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.views import generic
 
 from prezi.models import Prezi
 
-def prezi_list(request):
-    return render(request, 'prezi/prezi_list.html', {'prezis': Prezi.objects.all()})
+
+class PreziListView(generic.ListView):
+    template_name = 'prezi/prezi_list.html'
+    model = Prezi
+
+    def get_queryset(self):
+        self.query = self.request.GET.get('q')
+        if self.query:
+            qs = Prezi.objects.filter(title__icontains=self.query)
+        else:
+            qs = super(PreziListView, self).get_queryset()
+        return qs
+
+
+    def get_context_data(self, **kwargs):
+        context = super(PreziListView, self).get_context_data(**kwargs)
+        context['prezis'] = context['object_list'].order_by('created_on')
+        context['q'] = self.query
+        return context
+
+prezi_list = PreziListView.as_view()
