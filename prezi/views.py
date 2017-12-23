6533298ff35b99e913311906d7305b3031adcd3c
teglanna <teglanna@gmail.com>
2017-12-23 21:31:59 +0100

    Added pagination

--- prezi/views.py
+++ prezi/views.py
@@ -6,6 +6,7 @@ from prezi.models import Prezi
 class PreziListView(generic.ListView):
     template_name = 'prezi/prezi_list.html'
     model = Prezi
+    paginate_by = 24
 
     def get_queryset(self):
         self.query = self.request.GET.get('q')
@@ -13,12 +14,12 @@ class PreziListView(generic.ListView):
             qs = Prezi.objects.filter(title__icontains=self.query)
         else:
             qs = super(PreziListView, self).get_queryset()
-        return qs
+        return qs.order_by('created_on')
 
 
     def get_context_data(self, **kwargs):
         context = super(PreziListView, self).get_context_data(**kwargs)
-        context['prezis'] = context['object_list'].order_by('created_on')
+        context['prezis'] = context['object_list']
         context['q'] = self.query
         return context
 
